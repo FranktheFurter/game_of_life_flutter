74c994af4fb7fe56338d8afa71093d0e6d1eb743
Marc Frankfurter <marcfrankfurter@gmail.com>
2022-05-01 15:42:38 +0200

    implemented GoL logic

--- lib/main.dart
+++ lib/main.dart
@@ -126,7 +126,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-//myPainter
 class MyPainter extends CustomPainter {
   final List<List<bool>> _grid;
 
@@ -157,4 +156,45 @@ class MyPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+List<List<bool>> nextIteration(List<List<bool>> grid) {
+  var newGrid = List<List<bool>>.generate(
+    grid.length,
+    (i) => List<bool>.generate(
+      grid[i].length,
+      (j) => false,
+    ),
+  );
+
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[i].length; j++) {
+      var neighbors = 0;
+      for (var x = i - 1; x <= i + 1; x++) {
+        for (var y = j - 1; y <= j + 1; y++) {
+          if (x == i && y == j) {
+            continue;
+          }
+          if (x < 0 || x >= grid.length || y < 0 || y >= grid[i].length) {
+            continue;
+          }
+          if (grid[x][y]) {
+            neighbors++;
+          }
+        }
+      }
+      if (grid[i][j]) {
+        if (neighbors < 2 || neighbors > 3) {
+          newGrid[i][j] = false;
+        } else {
+          newGrid[i][j] = true;
+        }
+      } else {
+        if (neighbors == 3) {
+          newGrid[i][j] = true;
+        }
+      }
+    }
+  }
+  return newGrid;
 }
